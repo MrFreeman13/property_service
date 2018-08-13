e020ff1bf900f0cc61b00182cf95bbb055794276
Eugene Yevtushenko <freeman.zhenia@gmail.com>
2018-08-13 10:13:15 +0200

    Fixed serializer bug

--- app/serializers/property_serializer.rb
+++ app/serializers/property_serializer.rb
@@ -2,7 +2,7 @@ class PropertySerializer < ActiveModel::Serializer
   attributes :house_number, :street, :city, :zip_code, :lat, :lng
 
   attribute :price do
-    '%.2f' % @object.price
+    '%.2f' % @object.price.to_f
   end
 
   attribute :state do
--- spec/requests/properties_spec.rb
+++ spec/requests/properties_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe 'Properties API', type: :request do
       context 'when data exists for given params' do
         before do
           get '/properties',
-                       params: { lng: 52.533730, lat: 13.426110, property_type: :apartment, marketing_type: :sell }
+                       params: { lng: '52.533730', lat: '13.426110', property_type: :apartment, marketing_type: :sell }
         end
 
         it 'return properties in 5km radius' do
@@ -51,7 +51,7 @@ RSpec.describe 'Properties API', type: :request do
       context 'when data does not exist for given params' do
         it 'returns empty body when property out of radius' do
           get '/properties',
-              params: { lng: 52.533730, lat: 13.626110, property_type: :apartment, marketing_type: :sell }
+              params: { lng: '52.533730', lat: '13.626110', property_type: :apartment, marketing_type: :sell }
           expect(response.body).to be_empty
           expect(response).to have_http_status(204)
         end
@@ -59,14 +59,14 @@ RSpec.describe 'Properties API', type: :request do
 
         it 'returns empty body when does not match property type' do
           get '/properties',
-              params: { lng: 52.533730, lat: 13.426110, property_type: :single_family_house, marketing_type: :sell }
+              params: { lng: '52.533730', lat: '13.426110', property_type: :single_family_house, marketing_type: :sell }
           expect(response.body).to be_empty
           expect(response).to have_http_status(204)
         end
 
         it 'returns empty body when does not match marketing type' do
           get '/properties',
-              params: { lng: 52.533730, lat: 13.626110, property_type: :apartment, marketing_type: :rent }
+              params: { lng: '52.533730', lat: '13.626110', property_type: :apartment, marketing_type: :rent }
           expect(response.body).to be_empty
           expect(response).to have_http_status(204)
         end
@@ -77,7 +77,7 @@ RSpec.describe 'Properties API', type: :request do
       context 'when property type is invalid' do
         before do
           get '/properties',
-              params: { lng: 52.533730, lat: 13.426110, property_type: :castle, marketing_type: :sell }
+              params: { lng: '52.533730', lat: '13.426110', property_type: :castle, marketing_type: :sell }
         end
 
         it 'returns error message' do
@@ -92,7 +92,7 @@ RSpec.describe 'Properties API', type: :request do
       context 'when marketing type is invalid' do
         before do
           get '/properties',
-              params: { lng: 52.533730, lat: 13.426110, property_type: :apartment, marketing_type: :promo }
+              params: { lng: '52.533730', lat: '13.426110', property_type: :apartment, marketing_type: :promo }
         end
 
         it 'returns error message' do
@@ -107,7 +107,7 @@ RSpec.describe 'Properties API', type: :request do
       context 'when not all required params presented' do
         before do
           get '/properties',
-              params: { lng: 52.533730, lat: 13.426110, marketing_type: :sell }
+              params: { lng: '52.533730', lat: '13.426110', marketing_type: :sell }
         end
 
         it 'returns error message' do
